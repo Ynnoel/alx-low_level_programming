f32130d9814a401ef5bf683bc22bedce2b8bf716
Ynnoel <akuaasabea72@gmail.com>
2023-06-15 17:07:27 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
-#!/bin/bash
 #include<stdio.h>
+
 /**
  * main - Entry point
  *
@@ -9,5 +9,5 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}
 
+}
